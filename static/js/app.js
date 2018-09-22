fdd0ff4e28407f7f97ec14bf90b37f4987b3f75a
John Giglio <johngiglio@gmail.com>
2018-09-22 18:27:47 -0400

    Adding initial files

---
+++ static/js/app.js
@@ -0,0 +1,13 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+// Select the filter button
+var filter = d3.select("#filter-btn");
+
+filter.on("click", function() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+}
